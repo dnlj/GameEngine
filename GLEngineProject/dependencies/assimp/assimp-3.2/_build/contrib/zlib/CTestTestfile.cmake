46e087b1b3d43410b6ec91a014d097dedd996645
DanielJ <danielj@example.com>
2016-06-06 20:57:12 -0500

    Removed unnecessary folders
    Former-commit-id: d25248d3402d10b2a81816dde11d0534df238910 [formerly 0588ef8009c34408ae3ec99147757a1e4870bcc5]
    Former-commit-id: bf91c8a346c7e264f0d9fe68d755b2d7006970e4

--- GLEngineProject/dependencies/assimp/assimp-3.2/_build/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/OpenGL Projects/GLEngineSolution/GLEngineProject/dependencies/assimp/assimp-3.2/contrib/zlib
-# Build directory: D:/OpenGL Projects/GLEngineSolution/GLEngineProject/dependencies/assimp/assimp-3.2/_build/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
