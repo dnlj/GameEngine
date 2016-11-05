c6632008d9c1e7c6e2615a463efbd38536f67ae2
DanielJ <danielwjameson@yahoo.com>
2016-11-05 15:23:17 -0500

    Added documentation to util

--- GLEngineProject/include/engine/util/util.hpp
+++ GLEngineProject/include/engine/util/util.hpp
@@ -17,11 +17,54 @@
 
 
 namespace engine::util {
+
+	/**
+	 * Loads a file as a string.
+	 * 
+	 * @param[in] path The path to the file to load.
+	 * @return The contents of the file.
+	 */
 	std::string loadFile(const std::string &path);
+
+	/**
+	 * Prints out any OpenGL errors.
+	 * 
+	 * @return void
+	 */
 	void checkGLErrors();
+
+	/**
+	 * Prints OpenGl and related info.
+	 * 
+	 * @return void
+	 */
 	void printGLInfo();
+
+	/**
+	 * Loads OpenGL functions.
+	 * 
+	 * @return void
+	 */
 	void initializeOpenGL();
+
+	/**
+	 * Get the current date as a string.
+	 * 
+	 * @return The current date as a string.
+	 */
 	std::string getDateString();
+
+	/**
+	 * Converts a OpenGL type enumeration to a string.
+	 * 
+	 * @return The string representation of the enumeration.
+	 */
 	std::string typeEnumToString(const GLenum &type);
+
+	/**
+	 * Converts @p vec to a string.
+	 * 
+	 * @return The string representation of @p vec.
+	 */
 	std::string toString(const glm::vec3& vec);
 }
